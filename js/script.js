afb4ded80f9fe4243eb03ea119c129c0dca35b9c
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-02-23 20:31:44 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@ alert("Hello, world!");
 console.log("Hello from the console");
 alert("Thanks for visiting my website");
 document.write("<hl>Welcome to my website</hl>");
+
+// Take Home Challenge
+/**
+ *  Program Name: script.js
+ *   Description: Program demonstrates how to use JavaScript arguments.
+ * Related class: N/A
+ *         Input: N/A
+ *          args: N/A
+ *        Output: Name and favorite food.
+ *        Author: Brian Rodiles.
+ *                Copyright 2022, Brian Rodiles.
+ *                Version 1.0
+ **/
+console.log("Name: Brian Rodiles");
+console.log("Favorite Food: Tacos")
