37bc3f13d23976ee8f0f9c00ac434c8a175b1883
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-02-23 20:54:04 -0700

    First Commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-alert("Hello, world!");
-console.log("Hello from the console");
-alert("Thanks for visiting my website");
-document.write("<hl>Welcome to my website</hl>");
+alert("Warning! This message will self destruct in");
+alert("3 ...");
+alert("2 ...");
+alert("1 ...");
+// Whatever is on the p tag, change it to what is on double quotes
+document.querySelector("p").textContent = "🔥BOOM! That was easy!🔥";
+console.log("Message Destroyed!!");
