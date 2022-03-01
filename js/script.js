47ab7453b8035afe921baca08753572410242174
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-02-28 20:07:14 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-alert("Warning! This message will self destruct in");
-alert("3 ...");
-alert("2 ...");
-alert("1 ...");
-// Whatever is on the p tag, change it to what is on double quotes
-document.querySelector("p").textContent = "🔥BOOM! That was easy!🔥";
-console.log("Message Destroyed!!");
+let str = "Supercalifragilisticexpialidocious"
+let strLength = str.length
+console.log("Number of characters in " + str + ": " + strLength);
+console.log("Random number: " + 10);
+const bigNum = BigInt(92384023480)
+console.log("Big number: " + bigNum);
+console.log("Is the previous number of BigInt type? - " + (typeof bigNum === 'bigint'));
