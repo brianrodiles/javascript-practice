7953236915ec01dc07d3b3062a0c4833ecf9ed17
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-02-28 20:12:53 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ let strLength = str.length
 console.log("Number of characters in " + str + ": " + strLength);
 console.log("Random number: " + 10);
 const bigNum = BigInt(92384023480)
+console.log(typeof bigNum);
 console.log("Big number: " + bigNum);
 console.log("Is the previous number of BigInt type? - " + (typeof bigNum === 'bigint'));
