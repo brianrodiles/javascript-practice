3f770c1aa5f0663fbdd9148bc120ede8d9b33764
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-02 20:43:10 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-let userName = prompt("Please enter your first name:");
-let revName = userName.split("").reverse().join("");
-alert("This is the reversed name: " + revName);
-alert(userName);
+let firstName = prompt("Please enter your first name:");
+let lastName = prompt("Please enter your last name:");
+console.log("This is your first name: " + firstName.toUpperCase());
+alert("This is your last name: " + lastName.toLowerCase());
 
-let userNum1 = prompt("Please enter a number value:", "10");
-let userNum2 = prompt("Please enter another number value:", "10");
-let sumNums = parseInt(userNum1) + parseInt(userNum2);
-alert("Sum of two numbers: " + sumNums);
+let userBirth = prompt("Please enter your birthday in form mm/dd/yy:");
+let userPreference = "";
+let question = "So, is your birthday really on " + userBirth + "?";
+let i = 0;
+while(userPreference != "Yes"){
+    if(confirm(question) == true){
+        userPreference = "Yes";
+    }
+    else{
+        userPreference = "No";
+    }
+    if(userPreference == "No" && i % 2 == 0){
+        question = "";
+        question = "Are you kidding me?\n" + "So, is your birthday really on " + userBirth + "?";
+    }
+    if(userPreference == "No" && i % 2 != 0){
+        question = "";
+        question = "Be honest with me.\n" + "So, is your birthday really on " + userBirth + "?";
+    }
+
+    i++;
+}
+alert("Nice, I will assume your birthday is on " + userBirth + ".");
