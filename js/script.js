166992c76382ece31eb16a500d7977fdc4a9e924
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-02 19:04:16 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,61 +1,8 @@
-const str = 'JavaScript is fun';
-// .slice()
-console.log(str.slice());
-console.log(str.slice(4));
-console.log(str.slice(0, 10));
-// .toUpperCase() & .toLowerCase()
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-// .toCharAt(int x)
-console.log(str.charAt());
-console.log(str.charAt(11));
-console.log(str.charAt(14));
-console.log(str.charAt(110));
+let userName = prompt("Please enter your first name:");
+let revName = userName.split("").reverse().join("");
+alert("This is the reversed name: " + revName);
 
-const str2 = 'Javascript is amazing';
-const strNew = 'JavaScript-is-amazing';
-
-// Returns an array
-console.log(str2.split());
-console.log(str2.split('S'));
-console.log(str.split('is'));
-console.log(str.split(' '));
-console.log(strNew.split('-'));
-
-console.log(str.replace('JavaScript', 'Node.js'));
-// Using regular expressions (regex) for case insensitive
-console.log(str.replace(/Javascript/i, 'Node.js'));
-console.log(str.replace('a', 'A'));
-console.log(str.replace(/a/g, 'A'));
-
-const str3 = 'JavaScript';
-console.log(str3.repeat(3));
-console.log(str3.repeat(1));
-console.log(str3.repeat(0));
-
-const browserType = 'mozilla';
-console.log(browserType.length);
-
-// Parsing a String into an integer
-let num = "45";
-console.log(parseInt(num) + 50);
-
-// Pop Up Boxes
-alert("This is an alert message box.");
-alert('This number: ' + 100); // Displays with concatenation
-alert(100);
-alert(Date()); // Displays current date
-
-// Confirm box: If user clicks Ok, the conditional will be set to true.
-// In case the user presses "Cancel," it will be set to false.
-var userPreference;
-if(confirm("Do you want to save changes?") == true){
-    userPreference = "Data saved successfuly!";
-}
-else{
-    userPreference = "Save Cancelled!";
-}
-
-// Prompt box (for input). It returns null when pressing "Cancel".
-prompt("How old are you?", "30");
-
+let userNum1 = prompt("Please enter a number value:", "10");
+let userNum2 = prompt("Please enter another number value:", "10");
+let sumNums = parseInt(userNum1) + parseInt(userNum2);
+alert("Sum of two numbers: " + sumNums);
