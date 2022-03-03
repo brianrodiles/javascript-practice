6041434fe07bbf7d21e850747b27cf540f4902aa
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-02 19:06:08 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let userName = prompt("Please enter your first name:");
 let revName = userName.split("").reverse().join("");
 alert("This is the reversed name: " + revName);
+alert(userName);
 
 let userNum1 = prompt("Please enter a number value:", "10");
 let userNum2 = prompt("Please enter another number value:", "10");
