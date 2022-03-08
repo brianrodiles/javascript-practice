08e2e7df0966fe12bf6bd60d1077a2052f167923
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-07 20:45:45 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
-if(0 == false && 1 == true){
-    console.log("Comparing using general equals comparison.");
-}
-if(0 === false && 1 === true){
-    console.log("Comparing using strict comparison");
-}
-if(false != false && true != true){
-    console.log("Comparing using general not");
-}
-if(true !== false && false !== true){
-    console.log("Comparing using strict not");
-}
-// The debugger; is only used when wanting to stop the flow of a program
-// for debugging purposes.
-// debugger;
-if(1 != false && 0 != true){
-    console.log("Comparing using general not");
+function greatNum(){
+    let num1 = prompt("Enter a number:", 0);
+    let num2 = prompt("Enter another number:", 0);
+    intNum1 = parseInt(num1);
+    intNum2 = parseInt(num2);
+    if(intNum1 === intNum2){
+        alert("Both numbers are the same.");
+    }
+    else if(isNaN(intNum1) || isNaN(intNum2)){
+        alert("Please enter a value for both numbers.\nTry again...");
+        greatNum();
+    }
+    else if(intNum1 > intNum2){
+        alert(`${intNum1} is greater than ${intNum2}.`);
+    }
+    else{
+        alert(`${intNum2} is greater than ${intNum1}.`);
+    }
+    let sum = intNum1 + intNum2;
+    switch(sum % 2){
+        case 0:
+            alert(`The sum of both numbers, ${sum}, is a multiple of two.`);
+            break;
+        case 1:
+            alert(`The sum of both numbers, ${sum}, is not multiple of two.`);
+            break;
+    }
+    return(intNum1 > intNum2 ? intNum1 : intNum2);
 }
 
-// What can be compared with a false:
-// false, empty String, 0, null, NaN, undefined
+console.log(`The greatest of both numbers at the end was ${greatNum()}.`);
