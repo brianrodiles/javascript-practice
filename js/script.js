7c7d40542a2dc9b8e5ae8078d1dfa0cff2336037
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-09 19:58:11 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,4 +16,57 @@ let tNum = "100";
 let testNum = parseInt(tNum) + 1;
 console.log(testNum, typeof(testNum));
 
+// Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution.
+// That is what allows a function to be called above or below of its declaration.
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+console.log(createCustomGreeting("Hello, a."));
+
 // Functions
+function createCustomGreeting(message){
+    const greeting = `Welcome, a message from our sponsors: ${message}`;
+    console.log(greeting);
+    // A function always expects a return statement.
+    return;
+}
+
+console.log(createCustomGreeting("Bye, xd."));
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function fullNameConverter(firstName, lastName){
+    let fullName = firstName + " " + lastName;
+    console.log(fullName);
+    return fullName;
+}
+
+fullNameConverter("Brian", "Rodiles");
+
+// Function expression
+// Used when doing event listening, and it is called an anonymous expression
+// This type of functions are not hoist friendly as they are not callable if they were not declared before.
+// convertFullName("Bruh", "xd"); // Cannot be used
+const convertFullName = function (firstName, lastName){
+    return `Hello ${firstName} ${lastName}!`
+}
+
+console.log(convertFullName("Brian", "Rodiles"));
+
+// Arrow functions
+const convertFullNameArrow = (firstName, lastName) =>{
+    return firstName + " " + lastName;
+}
+
+console.log(convertFullNameArrow("Brian", "Arrow"));
+
+// No need of parentheses as it is only one parameter
+//                 ⇩
+const greeting = message => "Greeting: " + message.toUpperCase();
+//                                                             ↑
+// No need of return statement when using single-lined arrow functions
+// IMPORTANT: Keyword this can't be used in an arrow function (i.e. this.message.toUpperCase()).
+// Think it as, I AM THE EXPRESSION, I AM THE "THIS";
+// For more info: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+console.log(greeting("Hello World!"))
+
+//Another example
+const secondGreeting = () => "Hello World!";
+console.log(secondGreeting());
