4deb80419be331847604b74f8736685a3e329a33
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-09 21:16:26 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
-
-
+function testPassword(){
+    let flag = false;
+    let warnMessage = "";
+    let password = "";
+    let i = 0;
+    do{
+        password = prompt("Please, enter a password with 6-20 characters.\nAlso, make sure that it starts with a letter:");
+        if(password == null){
+            alert("Please, enter a password.");
+        }
+        else{
+            if(password.length < 5 || password.length > 21){
+                warnMessage += `The password (${password}) is out of the 6-20 characters bound.`;
+                i++;
+            }
+            if(!(/[a-zA-Z]/).test(password.charAt(0))){
+                if(i != 0){
+                    warnMessage += `\nAlso... \n`;
+                }
+                warnMessage += `The first character (${password.charAt(0)}) of the password (${password}) is not a character.`;
+                i++;
+            }
+            if(i != 0){
+                alert(warnMessage + `\nPlease, try again...`);
+            }
+            else{
+                alert("Password accepted, thank you for using the service!");
+                flag = true;
+            }
+        }
+        i = 0;
+        warnMessage = "";
+    }while(!flag);
+    return password;
+}
+testPassword();
 
 // // Using single-line arrow function
 // // This will not function because of isEvenOrOdd being an arrow function
