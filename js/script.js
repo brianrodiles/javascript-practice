31424cb0f7ea68b3793ae6c2b0f3769a8d1bf988
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-14 20:20:17 -0600

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,67 @@
+// ========================= EXAMPLE 1 =========================
+function job() {
+    return new Promise(function(resolve, reject) {
+        reject();
+    });
+}
+
+let promise1 = job();
+
+promise1
+
+.then(function() {
+    console.log('Success 1');
+})
+
+.then(function() {
+    console.log('Success 2');
+})
+
+.then(function() {
+    console.log('Success 3');
+})
+
+.catch(function() {
+    console.log('Error 1');
+})
+
+.then(function() {
+    console.log('Success 4');
+});
+
+// ========================= EXAMPLE 2 =========================
+function job(state) {
+    return new Promise(function(resolve, reject) {
+        if (state) {
+            resolve('success');
+        } else {
+            reject('error');
+        }
+    });
+}
+
+let promise2 = job(true);
+
+promise2
+
+.then(function(data) {
+    console.log(data);
+
+    return job(false);
+})
+
+.catch(function(error) {
+    console.log(error);
+
+    return 'Error caught';
+})
+
+.then(function(data) {
+    console.log(data);
+
+    return job(true);
+})
+
+.catch(function(error) {
+    console.log(error);
+});
