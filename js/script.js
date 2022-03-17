82fd74bc21aaba0dc89584936d3a9e35add51e30
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-16 20:34:25 -0600

    Changes on both HTLM and JS files

--- js/script.js
+++ js/script.js
@@ -4,9 +4,23 @@
 // let result3 = fetch(url).then(response => response.json()).then(data => console.log(data.people.filter(name => console.log("name", name.name))));
 
 // JSON ==> JavaScript Object Notation
-let catUrl = 'https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1';
-let catResult = fetch(catUrl).then(response => response.json()).then(data => console.log("Here is the cat fact data: ", data));
-let catFact = fetch(catUrl).then(response => response.json()).then(data => alert(`Here is a new fact about cats:\n${data.text}`));
+function alertCatFact(){
+    let catUrl = 'https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1';
+    let catResult = fetch(catUrl).then(response => response.json()).then(data => console.log("Here is the cat fact data: ", data));
+    let catFact = fetch(catUrl).then(response => response.json()).then(data => alert(`Here is a new fact about cats:\n${data.text}`));
+}
+function mOver(obj){
+    obj.innerHTML = "Click me!";
+}
+
+function mOut(obj){
+    obj.innerHTML = "Get a fact about cats!";
+}
+
+document.getElementById("catFactBtn").addEventListener("click", alertCatFact);
+document.getElementById("catFactBtn").addEventListener("mouseover", function(){document.getElementById("catFactBtn").innerHTML = "Click me!";});
+document.getElementById("catFactBtn").addEventListener("mouseout", function(){document.getElementById("catFactBtn").innerHTML = "Get another fact about cats!";});
+
 
 // Extra (during review):
 // You can use POSTMAN to see what will be returned on a GET call before fetching.
