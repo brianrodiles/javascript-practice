15656b4b03000cb2a39a7dd306db19f7e142cdf7
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-16 19:25:40 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,86 +1,22 @@
-// Arrays
-const shoppingList = ['bread', 'butter', 'honey'];
-console.log(shoppingList);
-console.log(shoppingList[0]);
-console.log(shoppingList[2]);
-console.log(shoppingList[3]);
-
-let planets = [
-    'Sun',
-    'Mercury',
-    'Venus',
-    'Earth',
-    'Mars',
-    'Jupiter',
-    'Saturn',
-    'Uranus',
-    'Neptune'
-]
-
-console.log(planets.length);
-planets.push("Pluton");
-// Removes last element from array
-planets.pop();
-// Removes first element from array
-planets.shift();
-// We can also use .unshift()
-// A snipping tool
-console.log(planets.slice(2));
-console.log(planets.slice(2, 4));
-console.log(planets.slice(1, 5));
-console.log(planets.slice(-2));
-console.log(planets.slice(2, -1));
-
-const months = ['Jan', 'March', 'April', 'June'];
-//   Index || elements to replace || element
-months.splice(1, 0, 'Feb');
-months.splice(4, 1, 'May');
-
-
-
-for(let i = 0; i < planets.length; ++i){
-    console.log(planets[i] + " ");
-}
-
-const arr1 = ['a', 'b', 'c'];
-const arr2 = ['d', 'e', 'f'];
-const arr3 = arr1.concat(arr2);
-
-const newArray = new Array(10);
-newArray[0] = 'a';
-for(let i = 1; i < newArray.length; ++i){
-    newArray[i] = String.fromCharCode(newArray[i - 1].charCodeAt() + 1);
-    console.log(newArray[i - 1].charCodeAt());
-}
-
-console.log(newArray);
-
-// Loops
-// for...in iterates over all enumerable properties of an object.
-const book = {
-    title: 'JavaScript for Beginners',
-    price: '$9.99',
-    year: 2018,
-    publisher: 'Amazon, Inc'
-}
-
-for(const key in book){
-    console.log(`${key} --> ${book[key]}`);
-}
-
-// for...of iterates over arrays and other data structures, but not over objects.
-const names = ['Alex', 'Emma', 'Atta'];
-for(const name of names){
-    console.log(`Hey ${name}!`)
-}
-
-// Use break; to stop the flow of a loop
-let i = 0;
-while(i < 6){
-    if(i === 3){
-        break;
+function fizzbuzz(){
+    let message = "";
+    for(let i = 1; i <= 100; ++i){
+        message += i + " --> ";
+        if(i % 3 == 0 && i % 5 == 0){
+            message += "FizzBuzz";
+        }
+        else if(i % 3 == 0){
+            message += "Fizz";
+        }
+        else if(i % 5 == 0){
+            message += "Buzz";
+        }
+        else{
+            message += "___";
+        }
+        console.log(message);
+        message = "";
     }
-    ++i;
 }
 
-console.log(i);
+fizzbuzz();
