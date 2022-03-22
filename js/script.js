4773bd013d4aa8b897f9f70447b8c5985223c262
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-21 20:13:09 -0600

    Car functions

--- js/script.js
+++ js/script.js
@@ -1,92 +1,31 @@
-// Objects (as JS is a object-based programming language)
-// JS Objects have properties and methods
-// In JS arrays are objects, with, e.g., the property length.
-// JS Objects allows to store data in key value pairs or property value pairs.
-const objVacation = {
-    // Any type of data can be used as value
-    "place":"Yosemite",
-    "days":5,
-    "transportation":"car"
-};
-const firstLetter = "a";
-const dictionary = {
-    "a":["Apple","Attitude","Azalea"],
-    "b":["Baby","Bitter","Best"],
-    "c":["Cat","Cowgirl","Curly"]
-};
-// Accesing object properties
-// Bracket notation
-let daysOnVacation = objVacation["days"];
-console.log(`Duration of trip: ${daysOnVacation} days.`);
-// Variable method
-console.log(`Contents of "a": ${dictionary[firstLetter]}`);
-// Dot notation
-console.log(`Way of transportation: ${objVacation.transportation}.`);
-
-// Changing object properties
-daysOnVacation = objVacation.days = 6;
-console.log(`New duration of trip: ${daysOnVacation} days.`);
-
-// Delete Object Properties
-delete dictionary.c;
-
-// Checking if a Property is an object
-console.log(objVacation.hasOwnProperty("place"));
-console.log(objVacation.hasOwnProperty("weather"));
-
-// Complex Objects
-// * When an object contains more than one data type.
-// * When a variable contains more than one object.
-
-const myFaveBooks = [
+const car = [
     {
-        "title":"The Secret Life of Bees",
-        "author":"Sue Monk Kid",
-        "pubDate":2001,
-        "Genre":"Fiction"
+        "make":"Jeep",
+        "model":"Compass",
+        "year":2013,
+        "color":"black"
     },
     {
-        "title":"Rubyfruit Jungle",
-        "author":"Rita Mae Brown",
-        "pubDate":1973,
-        "Genre":"Fiction"
-    }
-]
-
-const myFaveMusic = [
+        "make":"Ford",
+        "model":"Bronco",
+        "year":2022,
+        "color":"white"
+    },
     {
-        "albumTitle":"Into the Trees",
-        "musician":"Zoe Keating",
-        "releaseDate":2010,
-        "songs":["Forest","Escape Artist","Optimist"]
-    }
-]
-
-const myNestedFaveMusic = [
+        "make":"Peugot",
+        "model":"308",
+        "year":2021,
+        "color":"green"
+    },
     {
-        "albumTitle":"Into the Trees",
-        "musician":"Zoe Keating",
-        "releaseDate":2010,
-        "songs":{
-                    "Forest":{
-                        "Length":"2:07",
-                        "Instruments":"Cello"
-                    },
-                    "Into the Trees":{
-                        "Length":"3:15",
-                        "Instruments":"Cello"
-                    }
-        }
+        "make":"Porshe",
+        "model":"Cayenne S",
+        "year":2020,
+        "color":"blue"
     }
 ]
 
-console.log(myNestedFaveMusic[0]["songs"]["Forest"]);
-
-const myFaveAlbums = {
-    "Into the Trees":{"musician":"Zoe Keating",
-                      "songs":["Into the Trees","Forest","Optimist"]},
-    "Oops! I Did It Again":{"musician":"Britney Spears",
-                            "songs":["Oops! I Did It Again","Sodapop"]},
-};
-
-console.log(myFaveAlbums["Into the Trees"]["songs"][0]);
+console.log("Info about cars:");
+for(let i = 0; i < 4; i++){
+    console.log(`Car ${i}: ${car[i]["make"]} ${car[i]["model"]} ${car[i]["year"]} in ${car[i]["color"]}.`);
+}
