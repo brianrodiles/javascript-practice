a326ddffc5e83b29c15a43cebfb7be233f671e34
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-21 20:36:59 -0600

    Changed HTML and JS files

--- js/script.js
+++ js/script.js
@@ -28,4 +28,8 @@ const car = [
 console.log("Info about cars:");
 for(let i = 0; i < 4; i++){
     console.log(`Car ${i}: ${car[i]["make"]} ${car[i]["model"]} ${car[i]["year"]} in ${car[i]["color"]}.`);
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(`Car ${i}: ${car[i]["make"]} ${car[i]["model"]} ${car[i]["year"]} in ${car[i]["color"]}.`));
+    ul.appendChild(li);
 }
