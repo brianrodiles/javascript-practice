595a0f4964418092202de399e9e5bfc9e14b2c2f
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-23 20:17:29 -0600

    Changed HTML and JS files

--- js/script.js
+++ js/script.js
@@ -1,92 +1,23 @@
-// Objects (as JS is a object-based programming language)
-// JS Objects have properties and methods
-// In JS arrays are objects, with, e.g., the property length.
-// JS Objects allows to store data in key value pairs or property value pairs.
-const objVacation = {
-    // Any type of data can be used as value
-    "place":"Yosemite",
-    "days":5,
-    "transportation":"car"
-};
-const firstLetter = "a";
-const dictionary = {
-    "a":["Apple","Attitude","Azalea"],
-    "b":["Baby","Bitter","Best"],
-    "c":["Cat","Cowgirl","Curly"]
-};
-// Accesing object properties
-// Bracket notation
-let daysOnVacation = objVacation["days"];
-console.log(`Duration of trip: ${daysOnVacation} days.`);
-// Variable method
-console.log(`Contents of "a": ${dictionary[firstLetter]}`);
-// Dot notation
-console.log(`Way of transportation: ${objVacation.transportation}.`);
+const userName = document.getElementById('userName');
+const psswd = document.getElementById('psswd');
+const form = document.getElementById('form');
+const errorMessage = document.getElementById('errorMessage');
 
-// Changing object properties
-daysOnVacation = objVacation.days = 6;
-console.log(`New duration of trip: ${daysOnVacation} days.`);
-
-// Delete Object Properties
-delete dictionary.c;
-
-// Checking if a Property is an object
-console.log(objVacation.hasOwnProperty("place"));
-console.log(objVacation.hasOwnProperty("weather"));
-
-// Complex Objects
-// * When an object contains more than one data type.
-// * When a variable contains more than one object.
-
-const myFaveBooks = [
-    {
-        "title":"The Secret Life of Bees",
-        "author":"Sue Monk Kid",
-        "pubDate":2001,
-        "Genre":"Fiction"
-    },
-    {
-        "title":"Rubyfruit Jungle",
-        "author":"Rita Mae Brown",
-        "pubDate":1973,
-        "Genre":"Fiction"
+form.addEventListener('submit', (e) => {
+    let message = '';
+    if(userName.value === '' || userName.value == null){
+        message = 'Please fill out all the fields on the form.';
     }
-]
-
-const myFaveMusic = [
-    {
-        "albumTitle":"Into the Trees",
-        "musician":"Zoe Keating",
-        "releaseDate":2010,
-        "songs":["Forest","Escape Artist","Optimist"]
+    else if(psswd.value === '' || psswd.value == null){
+        message = 'Please fill out all the fields on the form.';
     }
-]
-
-const myNestedFaveMusic = [
-    {
-        "albumTitle":"Into the Trees",
-        "musician":"Zoe Keating",
-        "releaseDate":2010,
-        "songs":{
-                    "Forest":{
-                        "Length":"2:07",
-                        "Instruments":"Cello"
-                    },
-                    "Into the Trees":{
-                        "Length":"3:15",
-                        "Instruments":"Cello"
-                    }
-        }
+    else if(userName.value !== 'admin' || psswd.value !== '132'){
+        message = 'Incorrect credentials, please try again.';
     }
-]
-
-console.log(myNestedFaveMusic[0]["songs"]["Forest"]);
-
-const myFaveAlbums = {
-    "Into the Trees":{"musician":"Zoe Keating",
-                      "songs":["Into the Trees","Forest","Optimist"]},
-    "Oops! I Did It Again":{"musician":"Britney Spears",
-                            "songs":["Oops! I Did It Again","Sodapop"]},
-};
-
-console.log(myFaveAlbums["Into the Trees"]["songs"][0]);
+    e.preventDefault();
+    errorMessage.innerText = message;
+    if(errorMessage.innerText == ''){
+        document.getElementById('form').hidden = true;
+        document.getElementById('infoDisp').innerText = "Nice, remember that...\nyour username is [" + document.getElementById('userName').value + "],\n" + "and your password is [" + document.getElementById('psswd').value + "]."
+    }
+})
