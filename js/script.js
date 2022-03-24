a2983e427c156db5e7cfb918bea4a54433bc3517
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-23 18:37:31 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,92 +1,26 @@
-// Objects (as JS is a object-based programming language)
-// JS Objects have properties and methods
-// In JS arrays are objects, with, e.g., the property length.
-// JS Objects allows to store data in key value pairs or property value pairs.
-const objVacation = {
-    // Any type of data can be used as value
-    "place":"Yosemite",
-    "days":5,
-    "transportation":"car"
-};
-const firstLetter = "a";
-const dictionary = {
-    "a":["Apple","Attitude","Azalea"],
-    "b":["Baby","Bitter","Best"],
-    "c":["Cat","Cowgirl","Curly"]
-};
-// Accesing object properties
-// Bracket notation
-let daysOnVacation = objVacation["days"];
-console.log(`Duration of trip: ${daysOnVacation} days.`);
-// Variable method
-console.log(`Contents of "a": ${dictionary[firstLetter]}`);
-// Dot notation
-console.log(`Way of transportation: ${objVacation.transportation}.`);
+// Forms
+// <form action="/action_page.php" method="get" name="myForm">
+//   <label for="fname">First name:</label>
+//   <input type="text" id="fname" name="fname"><br><br>
+//   <label for="lname">Last name:</label>
+//   <input type="text" id="lname" name="lname"><br><br>
+//   <input type="button" onclick="formSubmit()" value="Send form data!">
+// </form>
 
-// Changing object properties
-daysOnVacation = objVacation.days = 6;
-console.log(`New duration of trip: ${daysOnVacation} days.`);
+// <input> value Attribute (Forms to create placeholders)
+// <form action="/action_page.php">
+//   <label for="fname">First name:</label>
+//   <input type="text" id="fname" name="fname" value="John"><br><br>
+//   <label for="lname">Last name:</label>
+//   <input type="text" id="lname" name="lname" value="Doe"><br><br>
+//   <input type="submit" value="Submit">
+// </form>
 
-// Delete Object Properties
-delete dictionary.c;
-
-// Checking if a Property is an object
-console.log(objVacation.hasOwnProperty("place"));
-console.log(objVacation.hasOwnProperty("weather"));
-
-// Complex Objects
-// * When an object contains more than one data type.
-// * When a variable contains more than one object.
-
-const myFaveBooks = [
-    {
-        "title":"The Secret Life of Bees",
-        "author":"Sue Monk Kid",
-        "pubDate":2001,
-        "Genre":"Fiction"
-    },
-    {
-        "title":"Rubyfruit Jungle",
-        "author":"Rita Mae Brown",
-        "pubDate":1973,
-        "Genre":"Fiction"
-    }
-]
-
-const myFaveMusic = [
-    {
-        "albumTitle":"Into the Trees",
-        "musician":"Zoe Keating",
-        "releaseDate":2010,
-        "songs":["Forest","Escape Artist","Optimist"]
-    }
-]
-
-const myNestedFaveMusic = [
-    {
-        "albumTitle":"Into the Trees",
-        "musician":"Zoe Keating",
-        "releaseDate":2010,
-        "songs":{
-                    "Forest":{
-                        "Length":"2:07",
-                        "Instruments":"Cello"
-                    },
-                    "Into the Trees":{
-                        "Length":"3:15",
-                        "Instruments":"Cello"
-                    }
-        }
-    }
-]
-
-console.log(myNestedFaveMusic[0]["songs"]["Forest"]);
-
-const myFaveAlbums = {
-    "Into the Trees":{"musician":"Zoe Keating",
-                      "songs":["Into the Trees","Forest","Optimist"]},
-    "Oops! I Did It Again":{"musician":"Britney Spears",
-                            "songs":["Oops! I Did It Again","Sodapop"]},
-};
-
-console.log(myFaveAlbums["Into the Trees"]["songs"][0]);
+// <form> action Attribute (where information goes after submittion)
+// <form action="/action_page.php" method="get">
+//   <label for="fname">First name:</label>
+//   <input type="text" id="fname" name="fname"><br><br>
+//   <label for="lname">Last name:</label>
+//   <input type="text" id="lname" name="lname"><br><br>
+//   <input type="submit" value="Submit">
+// </form>
