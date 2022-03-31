07ac05672c51a856533a2f39255afd00e947afb9
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-31 00:22:00 -0600

    Added images

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ const question = document.getElementById("question");
 const form = document.getElementById("form");
 const errorMessage = document.getElementById("errorMessage");
 
+const arrImages = [];
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'imagesForRandom');
+fs.readdir(dirPath,(err,files)=>{
+    files.forEach((item)=>{
+        arrImages.push(item);
+    })
+})
+console.log(arrImages);
+
 form.addEventListener('submit', (e) => {
     let error = '';
     if(question.value === '' || question.value == null){
