64ac67f545632e532eaec41731136da6e25d2ccc
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-30 20:16:27 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,14 +17,29 @@ form.addEventListener('submit', (e) => {
     else if((psswd.value === '' || psswd.value == null) && (repPsswd.value !== '' || repPsswd.value != null)){
         message = 'Please provide a password before confirming.';
     }
-    // else if((psswd.value === '' || psswd.value == null) && (repPsswd.value === '' || repPsswd.value == null)){
-    //     message = 'Please fill out all the fields on the form.';
-    // }
-    else if(repPsswd.value === '' || repPsswd.value == null){
-        message = 'Please confirm your password.';
-    }
-    else if(psswd.value !== repPsswd.value){
-        message = 'Passwords do not match.';
+    // Checking if the password is between 6-20 characters
+    else if((psswd.value !== '' || psswd.value != null)){
+        if(!/^.{6,20}$/.test(psswd.value)){
+        message = '\nPassword should be between 6-20 characters.'
+        }
+        // Checking if the password has at least one numeric digit
+        if(!/.\d/.test(psswd.value)){
+            message += '\nPassword should have at least one numeric digit.'
+        }
+        // Checking if the password has at least one uppercase letter
+        if(!/.[A-Z]/.test(psswd.value)){
+            message += '\nPassword should have at least one uppercase letter.'
+        }
+        // Checking if the password has at least one lowercase letter
+        if(!/.[a-z]/.test(psswd.value)){
+            message += '\nPassword should have at least one lowercase letter.'
+        }
+        if(repPsswd.value === '' || repPsswd.value == null){
+            message += '\nPlease confirm your password.';
+        }
+        else if(psswd.value !== repPsswd.value){
+            message = 'Passwords do not match.';
+        }
     }
     e.preventDefault();
     errorMessage.innerText = message;
