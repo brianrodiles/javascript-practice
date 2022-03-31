3dbc8f030fbaa99681c26d31a627d4e0d63e7f67
Brian Rodiles <bgrodilesdelgado@gmail.com>
2022-03-30 20:33:17 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,3 +17,15 @@
 // Any character .   Matching any character
 
 // You can test regex at regexr.com
+
+// /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$
+//   (?=.*?[A-Z]) At least one upper case English letter
+//               (?=.*?[a-z]) At least one lower case English letter
+//                           (?=.*?[0-9]) At least one digit
+//                                       (?=.*?[#?!@$%^&*-]) At least one special character
+//                                                          .{8,} Minimum eight in length (with the anchors)
+
+// ^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,20}$/g
+
+// What does a question mark in regex do?
+// It matches zero or one of the previous character.
